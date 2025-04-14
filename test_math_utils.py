5c7524080ac408c9c1b00f31d83818ad0780b2ee
Luscaa-dev <lucas.morais.57900@a.fecaf.com.br>
2025-04-14 20:11:57 -0300

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -5,10 +5,13 @@ class TestMathUtils(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(3, 5), 8)
         self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(100, 36), 136)
+        self.assertEqual(add(5200, 4000), 9200)
 
     def test_subtract(self):
         self.assertEqual(subtract(10, 4), 6)
         self.assertEqual(subtract(0, 0), 0)
+        self.assertEqual(subtract(-20, -10), -30)
 
 if __name__ == '__main__':
     unittest.main()
